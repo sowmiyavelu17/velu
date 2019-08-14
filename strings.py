2ee216bb8e5d0e8eb7289446a058490fd7dd509b
sowmiyavelu17 <53555335+sowmiyavelu17@users.noreply.github.com>
2019-08-14 12:25:50 +0530

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,9 @@
+my_string='Hello'
+print(my_string)
+my_string="Hello"
+print(my_string)
+my_string = '''Hello'''
+print(my_string)
+my_string = """Hello, welcome to
+           the world of Python"""
+print(my_string)
